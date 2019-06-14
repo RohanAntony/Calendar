809038008d99a14db8382d4794022b44ffd59d50
Rohan Antony <rohan271995@gmail.com>
2019-06-14 22:10:12 +0530

    Added support to display month from a list of month names

--- calendar/src/components/Calendar/calendar.js
+++ calendar/src/components/Calendar/calendar.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import DaysHeading from '../DaysHeading/daysHeading';
+import DaysHeading from '../Heading/daysHeading';
+import MonthHeading from '../Heading/monthHeading';
 import Week from '../Week/week';
 
 class Calendar extends Component{
+
+	constructor(props){
+		super(props)
+		let year = this.props.year, month = this.props.month;
+		// let first_date = new Date(year, month, 1)
+		// let last_date = new Date(year, month + 1, 0)
+		this.state = {
+			month: parseInt(month, 10),
+			year: parseInt(year, 10)
+		}
+	}
+
 	render() {
 		return (
 			<table className="calendar">
-				<DaysHeading />
-				<Week/>
+				<thead>
+					<MonthHeading month={this.state.month} year={this.state.year}/>
+					<DaysHeading />
+				</thead>
+				<tbody>
+					<Week/>
+				</tbody>
 			</table>
 		)
 	}
